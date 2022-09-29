8acd8287fed90d1187b49e007d524953b2d927f2
payaltank <payaltank_04@hotmail.co.uk>
2022-09-30 00:50:23 +0100

    Add project files.

---
+++ APL_Test2/Controllers/UploadImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APL_Test2.Controllers
+{
+    public class UploadImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ APL_Test2/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web;
+
+namespace APL_Test2.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }   
+        public string FilePath { get; set; }
+        public HttpPostedFileBase
+
+    }
+}
